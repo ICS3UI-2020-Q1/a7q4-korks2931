e11e7520b26dc2ebd62057a15942340ea6aa574b
korks2931 <korks2931@wrdsb.ca>
2020-10-13 15:41:31 +0000

    finished a7q4

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
+
 /**
- *
- * @author 
+ * This program will print out the last digit of the number in the method
+ * @author Sevde 
  */
 public class Main {
-
+ // make a method to find the last digit of the number 
+ public static int lastDigit ( int integer){
+   // formula to find the last digit of a positive number 
+   int lastOne = integer % 10; 
+   // if statement for the negative number
+   if (lastOne < 0){
+     // change the negative number back to positive by multiplying with -1
+     lastOne = lastOne * -1; 
+    
+   }
+   // print out the last digit 
+   System.out.println(lastOne);
+   // return the method 
+    return lastOne;
+   
+   
+ }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // do the method 
+   lastDigit(-947);
     
   }
 }
